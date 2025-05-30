7c7ebf3430eef6115b8bbbfe8a56375db1926a56
Jose Vidal Ordoñez <31360576+vidalordonezhn@users.noreply.github.com>
2025-05-29 21:53:01 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'template-angular-tail';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './modules/layout/layout.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { FormsComponent } from './modules/forms/forms.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: DashboardComponent },
+      { path: 'forms', component: FormsComponent },
+      { path: '**', redirectTo: 'dashboard' },
+    ],
+  },
+];
